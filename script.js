67b8e90975b11eeb61beeb065aa567d479abb8bc
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-12-17 13:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const answer = document.getElementById('ans');
+let str = "";
+
+for(let i=1;i<=100;i++){
+	 if(i % 3===0 && i % 5==0){
+		str = str + "FizzBuzz "
+	}
+	else if(i % 3 === 0){
+		str = str + "Fizz ";
+	}
+	else if(i % 5==0){
+		str = str+"Buzz "
+	}
+	else{
+		str = str + i + " ";
+	}
+}
+answer.innerText(str);
